af3e4eb4f3b7180d24d7e2b358c5fd5dfcd0daca
Spandan Chavan <spandanchavan727477@gmail.com>
2026-01-19 02:55:41 +0530

    base

---
+++ astro.config.mjs
@@ -0,0 +1,19 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import mdx from '@astrojs/mdx';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://spandan7724.github.io',
+  base: '/blog',
+  integrations: [mdx()],
+  markdown: {
+    shikiConfig: {
+      themes: {
+        light: 'github-light',
+        dark: 'github-dark',
+      },
+    },
+  },
+});
